1d67f23ed273459166d2f0f34bfa172b7e041e05
Brunokrk <45456467+Brunokrk@users.noreply.github.com>
2021-04-05 22:24:56 -0300

    .

--- avl_btree/main.c
+++ avl_btree/main.c
@@ -1,14 +1,26 @@
 #include "btree/btree.h"
+#define PIOR_CASO "arqs//pior_caso.txt"
 
 //PIOR CASO -> valores ordenados
 //MEDIO CASO -> valores aleatórios
 
 int main()
 {
+    char valor_str[4];
     ArvoreB *arvoreB = criaArvore(1);
+    FILE *pior_caso;
+    pior_caso = fopen(PIOR_CASO, "r+");
 
+    while (fgets(valor_str, 4, pior_caso) != NULL)
+    {
+        //printf("%d", atoi(valor_str));
+        if(atoi(valor_str)!=0){
+            adicionaChave(arvoreB,atoi(valor_str) );
+        }
+    }
+
+    fclose(pior_caso);
     percorreArvore(arvoreB->raiz);
     printf("Operacoes: %d", arvoreB->contador);
     printf("\n");
-    
 }
--- avl_btree/scriptsPython/piorcaso.py
+++ avl_btree/scriptsPython/piorcaso.py
@@ -1,6 +1,11 @@
 import random
+arquivo = open("arqs//pior_caso.txt", 'w')
 
 values = []
-
 for i in range(100):
-    print(i);
+    values.append(random.randint(1,1000))
+values.sort()
+
+for i in values:
+    arquivo.write(str(i)+"\n")
+
